10a321efa746f6d6457dd8ef511e59aa078c89be
farhysbb <farhiya24fsb@gmail.com>
2025-11-12 16:25:05 +0700

    sidebarr

--- resources/views/pages/beranda.blade.php
+++ resources/views/pages/beranda.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.appPegawai')
 
 {{-- 
   Data dummy untuk daftar perjalanan.
--- resources/views/pimpinan/beranda.blade.php
+++ resources/views/pimpinan/beranda.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.appPimpinan')
 
 @section('title', 'Beranda Pimpinan')
 
