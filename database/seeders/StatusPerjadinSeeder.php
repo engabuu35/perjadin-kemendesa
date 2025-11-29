2aa163dcd401dd985f65050a6b127e4619c8b38c
razaimoet <razzforai@gmail.com>
2025-11-30 00:02:36 +0700

    Perbaikan alur dari awal sampe akhir

--- database/seeders/StatusPerjadinSeeder.php
+++ database/seeders/StatusPerjadinSeeder.php
@@ -17,9 +17,11 @@ class StatusPerjadinSeeder extends Seeder
             ['nama_status' => 'Belum Berlangsung'], //jika tanggal belum mulai
             ['nama_status' => 'Sedang Berlangsung'], //jika tanggal sudah mulai
             ['nama_status' => 'Pembuatan Laporan'],  //jika perjalanan dinas selesai, laporan keungan pic
+            ['nama_status' => 'Menunggu Verifikasi Laporan'], //jika laporan sudah dikirim ke pic, menunggu verifikasi pic
             ['nama_status' => 'Menunggu Validasi PPK'], //jika laporan sudah dikirim ke ppk, menunggu validasi ppk
             ['nama_status' => 'Selesai'], //jika laporan divalidasi ppk
-            ['nama_status' => 'Perlu Tindakan'], //jika ada salah dan ppk minta revisi
+            // ['nama_status' => 'Perlu Tindakan'], //jika ada salah dan ppk minta revisi
+            ['nama_status' => 'Perlu Revisi'], //jika ada salah dan ppk minta revisi
             ['nama_status' => 'Diselesaikan Manual'],
             ['nama_status' => 'Dibatalkan'],
         ];
