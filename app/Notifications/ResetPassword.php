8850aca329160c99b57e93bbb70cdf78cb467bb1
engabuu35 <arbudibudibudi@gmail.com>
2025-12-08 20:16:39 +0700

    mentahan reset password

---
+++ app/Notifications/ResetPassword.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPassword extends Notification
+{
+    public $token;
+
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    public function toMail($notifiable)
+    {
+        $url = url(route('password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->email
+        ], false));
+
+        return (new MailMessage)
+            ->subject('Reset Password Akun Suspsys')
+            ->view('emails.reset-password', [
+                'name' => $notifiable->name ?? 'Pengguna',
+                'url'  => $url
+            ]);
+    }
+}
